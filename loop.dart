8dab1ee604a3907970d5c6ec5afe151384d91e70
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-10 23:50:55 +0600

    for loop

--- loop.dart
+++ loop.dart
@@ -13,4 +13,24 @@ List<String> fruits = ['Apple', 'Banana', 'Cherry', 'Date'];
 var GeeksForGeeks = [1,2,3,4,5];
   GeeksForGeeks.forEach((var num)=> print(num)); 
   
+
+  outerloop: // This is the label name 
+   
+   for (var i = 0; i < 5; i++) { 
+      print("Innerloop: ${i}"); 
+      innerloop: 
+      
+      for (var j = 0; j < 5; j++) { 
+         if (j > 3 ) break ; 
+         
+         // Quit the innermost loop 
+         if (i == 2) break innerloop; 
+         
+         // Do the same thing 
+         if (i == 4) break outerloop; 
+         
+         // Quit the outer loop 
+         print("Innerloop: ${j}"); 
+      } 
+   } 
  }
