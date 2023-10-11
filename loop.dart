beae16f129d7f407dd7c4f1fe7c253153cf6ad19
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-11 11:30:44 +0600

    while

--- loop.dart
+++ loop.dart
@@ -1,36 +1,37 @@
-void main(){
-
-for(int i=1;i<10;i++){
-print("dart");
-}
-List<String> fruits = ['Apple', 'Banana', 'Cherry', 'Date'];
+void main() {
+  for (int i = 1; i < 10; i++) {
+    print("dart");
+  }
+  List<String> fruits = ['Apple', 'Banana', 'Cherry', 'Date'];
 
   for (var fruit in fruits) {
     print(fruit);
   }
-  
 
-var GeeksForGeeks = [1,2,3,4,5];
-  GeeksForGeeks.forEach((var num)=> print(num)); 
-  
+  var GeeksForGeeks = [1, 2, 3, 4, 5];
+  GeeksForGeeks.forEach((var num) => print(num));
 
-  outerloop: // This is the label name 
-   
-   for (var i = 0; i < 5; i++) { 
-      print("Innerloop: ${i}"); 
-      innerloop: 
-      
-      for (var j = 0; j < 5; j++) { 
-         if (j > 3 ) break ; 
-         
-         // Quit the innermost loop 
-         if (i == 2) break innerloop; 
-         
-         // Do the same thing 
-         if (i == 4) break outerloop; 
-         
-         // Quit the outer loop 
-         print("Innerloop: ${j}"); 
-      } 
-   } 
- }
+  outerloop: // This is the label name
+
+  for (var i = 0; i < 5; i++) {
+    print("Innerloop: ${i}");
+    innerloop:
+    for (var j = 0; j < 5; j++) {
+      if (j > 3) break;
+
+      // Quit the innermost loop
+      if (i == 2) break innerloop;
+
+      // Do the same thing
+      if (i == 4) break outerloop;
+
+      // Quit the outer loop
+      print("Innerloop: ${j}");
+    }
+  }
+  int i = 1;
+  while (i <= 5) {
+    print(i);
+    i++;
+  }
+}
