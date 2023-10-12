27b4131d1c608f49a0035bd1ab6dcd3b7c71ca37
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-12 19:02:31 +0600

    continue

--- continueStatement.dart
+++ continueStatement.dart
@@ -9,4 +9,17 @@ void main() {
     count++;
   }
   print(" The count of odd values between 0 and 20 is: ${count}");
+
+  outerloop: // This is the label name
+
+  for (var i = 0; i < 3; i++) {
+    print("Outerloop:${i}");
+
+    for (var j = 0; j < 5; j++) {
+      if (j == 3) {
+        continue outerloop;
+      }
+      print("Innerloop:${j}");
+    }
+  }
 }
