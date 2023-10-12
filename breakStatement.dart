5b050da857b52c0972864076034ad2383e20da37
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-12 19:04:06 +0600

    break2

--- breakStatement.dart
+++ breakStatement.dart
@@ -5,4 +5,22 @@ void main() {
       break; // Exit the loop once the first even number is found
     }
   }
+  outerloop: // This is the label name
+
+  for (var i = 0; i < 5; i++) {
+    print("Innerloop: ${i}");
+    innerloop:
+    for (var j = 0; j < 5; j++) {
+      if (j > 3) break;
+
+      // Quit the innermost loop
+      if (i == 2) break innerloop;
+
+      // Do the same thing
+      if (i == 4) break outerloop;
+
+      // Quit the outer loop
+      print("Innerloop: ${j}");
+    }
+  }
 }
