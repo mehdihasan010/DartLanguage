436181a40cd05c7834ed3d61430f3949aadfdf80
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-14 18:54:18 +0600

    class & object

--- classes.dart
+++ classes.dart
@@ -1,10 +1,22 @@
 void main() {
   var obj = pet();
   obj.display();
+  print(obj.name);
+  var obj1 = new myPet();
+  obj1.name = 'mini';
+  obj1.display();
 }
 
 class pet {
+  var name = 'mini';
   void display() {
     print('Cat');
+  }
+}
+
+class myPet {
+  var name;
+  void display() {
+    print('My Cat Name Is ' + name);
   }
 }
