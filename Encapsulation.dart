9bfc317e7a3659de2f9462993009d37bd0722deb
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-16 21:13:08 +0600

    Access Modifiers

--- Encapsulation.dart
+++ Encapsulation.dart
@@ -1,8 +1,10 @@
-class Car {
-  late String model; // Public member
-}
-
 void main() {
   var myCar = Car();
   myCar.model = 'Toyota'; // Accessible because model is public
+  myCar._engine = 'v8';
+}
+
+class Car {
+  late String model; // Public member
+  late String _engine;
 }
