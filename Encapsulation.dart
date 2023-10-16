4bb215575dbb98c39b32af06f1abc10f887ee7d3
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-16 21:14:06 +0600

    Getters and Setters

--- Encapsulation.dart
+++ Encapsulation.dart
@@ -2,9 +2,30 @@ void main() {
   var myCar = Car();
   myCar.model = 'Toyota'; // Accessible because model is public
   myCar._engine = 'v8';
+  var temp = Temperature();
+  temp.celsius = 25; // Calls the setter
+  print(temp.celsius); // Calls the getter: Output: 25.0
+
+  temp.celsius = -300; // Output: Invalid temperature.
 }
 
 class Car {
   late String model; // Public member
   late String _engine;
+}
+
+class Temperature {
+  late double _celsius;
+
+  // Getter
+  double get celsius => _celsius;
+
+  // Setter
+  set celsius(double value) {
+    if (value >= -273.15) {
+      _celsius = value;
+    } else {
+      print('Invalid temperature.');
+    }
+  }
 }
