37a43cf654e627cce04e6e674de6f7e0238eb03d
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-22 21:54:04 +0600

    mixin keyword

--- Mixin.dart
+++ Mixin.dart
@@ -1,3 +1,14 @@
+void main() {
+  var bird = Bird();
+  bird.fly();
+  bird.walk();
+
+  var human = Human();
+  human.walk();
+  var dog = Dog('My Dog', 25);
+  dog.run();
+}
+
 mixin CanFly {
   void fly() {
     print('I can fly');
@@ -14,11 +25,26 @@ class Bird with CanFly, CanWalk {}
 
 class Human with CanWalk {}
 
-void main() {
-  var bird = Bird();
-  bird.fly();
-  bird.walk();
+abstract class Animal {
+  // properties
+  String name;
+  double speed;
 
-  var human = Human();
-  human.walk();
+  // constructor
+  Animal(this.name, this.speed);
+
+  // abstract method
+  void run();
+}
+
+// mixin CanRun is only used by class that extends Animal
+mixin CanRun on Animal {
+  // implementation of abstract method
+  @override
+  void run() => print('$name is Running at speed $speed');
+}
+
+class Dog extends Animal with CanRun {
+  // constructor
+  Dog(String name, double speed) : super(name, speed);
 }
