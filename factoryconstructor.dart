37f0160cc4d52390b33567d8f7f0f3c46a6f0624
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-24 22:19:43 +0600

    factory constructor Person

--- factoryconstructor.dart
+++ factoryconstructor.dart
@@ -1,27 +1,26 @@
-class Area {
-  final int length;
-  final int breadth;
-  final int area;
+class Person {
+  String firstName;
+  String lastName;
 
-  // private constructor
-  const Area._internal(this.length, this.breadth) : area = length * breadth;
+  // constructor
+  Person(this.firstName, this.lastName);
 
-  // Factory constructor
-  factory Area(int length, int breadth) {
-    if (length < 0 || breadth < 0) {
-      throw Exception("Length and breadth must be positive");
-    }
-    // redirect to private constructor
-    return Area._internal(length, breadth);
+  // factory constructor Person.fromMap
+  factory Person.fromMap(Map<String, Object> map) {
+    final firstName = map['firstName'] as String;
+    final lastName = map['lastName'] as String;
+    return Person(firstName, lastName);
   }
 }
 
 void main() {
-  // This works
-  Area area = Area(10, 20);
-  print("Area is: ${area.area}");
+  // create a person object
+  final person = Person('John', 'Doe');
 
-  // notice that here is negative value
-  Area area2 = Area(-10, 20);
-  print("Area is: ${area2.area}");
+  // create a person object from map
+  final person2 = Person.fromMap({'firstName': 'Harry', 'lastName': 'Potter'});
+
+  // print first and last name
+  print("From normal constructor: ${person.firstName} ${person.lastName}");
+  print("From factory constructor: ${person2.firstName} ${person2.lastName}");
 }
