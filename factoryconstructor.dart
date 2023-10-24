72963120da85a2df2c682176b58379e0c59ee819
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-24 22:18:38 +0600

    Factory constructor

--- factoryconstructor.dart
+++ factoryconstructor.dart
@@ -3,15 +3,25 @@ class Area {
   final int breadth;
   final int area;
 
-  // Initializer list
-  const Area(this.length, this.breadth) : area = length * breadth;
+  // private constructor
+  const Area._internal(this.length, this.breadth) : area = length * breadth;
+
+  // Factory constructor
+  factory Area(int length, int breadth) {
+    if (length < 0 || breadth < 0) {
+      throw Exception("Length and breadth must be positive");
+    }
+    // redirect to private constructor
+    return Area._internal(length, breadth);
+  }
 }
 
 void main() {
+  // This works
   Area area = Area(10, 20);
   print("Area is: ${area.area}");
 
-  // notice that here is a negative value
+  // notice that here is negative value
   Area area2 = Area(-10, 20);
   print("Area is: ${area2.area}");
 }
