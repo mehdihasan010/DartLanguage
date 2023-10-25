70d2280116f98a2aab9ff22cf3cbd6b15940aef5
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-25 22:13:52 +0600

    gen

---
+++ Generics.dart
@@ -0,0 +1,21 @@
+// Without Generics
+// Creating a class for int
+class IntData {
+  int data;
+  IntData(this.data);
+}
+
+// Creating a class for double
+class DoubleData {
+  double data;
+  DoubleData(this.data);
+}
+
+void main() {
+  // Create an object of IntData class
+  IntData intData = IntData(10);
+  DoubleData doubleData = DoubleData(10.5);
+  // Print the data
+  print("IntData: ${intData.data}");
+  print("DoubleData: ${doubleData.data}");
+}
