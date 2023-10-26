fc2c1071cc14a7c7afa37c5adc6b96190681ba3f
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-26 16:06:49 +0600

    Use Nullable Variables

--- NullSafety.dart
+++ NullSafety.dart
@@ -5,4 +5,14 @@ void main() {
   name = "John";
 // Assigning null to name
   name = null;
+// Checking if name is null using if statement
+  if (name == null) {
+    print("Name is null");
+  }
+// Using ?? operator to assign a default value
+  String name1 = name ?? "Stranger";
+  print(name1);
+// Using ! operator to return null if name is null
+  String name2 = name!;
+  print(name2);
 }
