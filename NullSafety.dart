ee6dd8f642c8821b19d6ac14069dfb7fe9c93570
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-26 16:09:17 +0600

    Function With Nullable

--- NullSafety.dart
+++ NullSafety.dart
@@ -18,4 +18,9 @@ void main() {
   // list of nullable ints
   List<int?> items = [1, 2, null, 4];
   print(items);
+  printAddress(null);
+}
+
+void printAddress(String? address) {
+  print(address);
 }
