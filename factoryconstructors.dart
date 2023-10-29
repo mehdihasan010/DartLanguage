ed63f0a82077e3cfeb5ad39797d0990c0240b998
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-29 23:58:51 +0600

    factory constructor

--- factoryconstructors.dart
+++ factoryconstructors.dart
@@ -16,6 +16,21 @@ class Area {
   }
 }
 
+class Person {
+  String firstName;
+  String lastName;
+
+  // constructor
+  Person(this.firstName, this.lastName);
+
+  // factory constructor Person.fromMap
+  factory Person.fromMap(Map<String, Object> map) {
+    final firstName = map['firstName'] as String;
+    final lastName = map['lastName'] as String;
+    return Person(firstName, lastName);
+  }
+}
+
 void main() {
   // This works
   Area area = Area(10, 20);
@@ -24,4 +39,13 @@ void main() {
   // notice that here is negative value
   Area area2 = Area(-10, 20);
   print("Area is: ${area2.area}");
+  // create a person object
+  final person = Person('John', 'Doe');
+
+  // create a person object from map
+  final person2 = Person.fromMap({'firstName': 'Harry', 'lastName': 'Potter'});
+
+  // print first and last name
+  print("From normal constructor: ${person.firstName} ${person.lastName}");
+  print("From factory constructor: ${person2.firstName} ${person2.lastName}");
 }
