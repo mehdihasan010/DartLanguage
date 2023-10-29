bb5e5b5e84e61cf5ac793edf341737425073bc8d
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-29 23:59:24 +0600

    emmu

--- factoryconstructors.dart
+++ factoryconstructors.dart
@@ -31,6 +31,42 @@ class Person {
   }
 }
 
+// enum ShapeType
+enum ShapeType { circle, rectangle }
+
+// abstract class Shape
+abstract class Shape {
+  // factory constructor
+  factory Shape(ShapeType type) {
+    switch (type) {
+      case ShapeType.circle:
+        return Circle();
+      case ShapeType.rectangle:
+        return Rectangle();
+      default:
+        throw 'Invalid shape type';
+    }
+  }
+  // method
+  void draw();
+}
+
+class Circle implements Shape {
+  // implement draw method
+  @override
+  void draw() {
+    print('Drawing circle');
+  }
+}
+
+class Rectangle implements Shape {
+  // implement draw method
+  @override
+  void draw() {
+    print('Drawing rectangle');
+  }
+}
+
 void main() {
   // This works
   Area area = Area(10, 20);
