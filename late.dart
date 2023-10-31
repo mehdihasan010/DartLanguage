070e26d89fa2e9ffaa70e8ae6e71b4ab4685c089
mehdihasan010 <mdmehdicse37@gmail.com>
2023-10-31 22:08:02 +0600

    Late Keyword

--- late.dart
+++ late.dart
@@ -5,4 +5,17 @@ void main() {
   // assigning value to late variable
   name = "John";
   print(name);
+  Person person = Person();
+  // late variable is initialized here
+  person.name = "John";
+  person.greet();
+}
+
+class Person {
+  // late variable
+  late String name;
+
+  void greet() {
+    print("Hello $name");
+  }
 }
