fa69516bc8857741d1a6710faa7e69302dd18815
mehdihasan010 <mdmehdicse37@gmail.com>
2023-11-03 22:28:20 +0600

    class

--- classes.dart
+++ classes.dart
@@ -23,3 +23,28 @@ class myPet {
     print('My Cat Name Is ' + name);
   }
 }
+
+class Student {
+  String name = "hello";
+  int age = 25;
+
+  String get stud_name {
+    return name;
+  }
+
+  void set stud_name(String name) {
+    this.name = name;
+  }
+
+  void set stud_age(int age) {
+    if (age <= 0) {
+      print("Age should be greater than 5");
+    } else {
+      this.age = age;
+    }
+  }
+
+  int get stud_age {
+    return age;
+  }
+}
