809b6e38ecf94689405f8bbda6f1719c1fd22b02
mehdihasan010 <mdmehdicse37@gmail.com>
2023-11-07 22:11:29 +0600

    input loop

--- input.dart
+++ input.dart
@@ -4,4 +4,8 @@ void main() {
   print('What is your name? ');
   var name = stdin.readLineSync();
   print('Hello, $name!');
+  int number = stdin.readByteSync();
+  for (int i = 1; i < number; i++) {
+    print(number);
+  }
 }
